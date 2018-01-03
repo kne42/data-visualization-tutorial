45f9bca0440214033fb3587efe78ec4b383511c0
Kira Evans <contact@kne42.me>
2018-01-03 13:48:20 -0800

    Fixed typo in setup script.

--- ci/travis/before_install.sh
+++ ci/travis/before_install.sh
@@ -5,7 +5,7 @@ download () { curl $1 > install.sh; }
 
 section_begin () { echo -en "travis_fold:start:$1\r"; }
 
-section_end () { echo -en "travis_fold:end$1\r"; }
+section_end () { echo -en "travis_fold:end:$1\r"; }
 
 export -f download
 export -f section_begin
