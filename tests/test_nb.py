c7a6facde400fe004667773ef30270e1f8c92d56
Kira Evans <contact@kne42.me>
2018-01-03 18:10:47 -0800

    Use dummy notebook instead of a temporary file.

--- tests/test_nb.py
+++ tests/test_nb.py
@@ -1,15 +1,13 @@
 # credit: https://github.com/ghego/travis_anaconda_jupyter
 
 import subprocess
-import tempfile
 
 
 def _exec_notebook(path):
-    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
-        args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
-                "--ExecutePreprocessor.timeout=1000",
-                "--output", fout.name, path]
-        subprocess.check_call(args)
+    args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
+            "--ExecutePreprocessor.timeout=1000",
+            "--output", "../dummy.ipynb", path]
+    subprocess.check_call(args)
 
 
 def test():
